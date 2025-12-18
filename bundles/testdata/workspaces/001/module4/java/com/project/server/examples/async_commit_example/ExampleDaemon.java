811297af9723f080bfcdca29ce47584e9b57c3d4
runchen0919 <run.chen@compass.com>
2025-12-18 16:51:00 +0800

    Add test cases

---
+++ bundles/testdata/workspaces/001/module4/java/com/project/server/examples/async_commit_example/ExampleDaemon.java
@@ -0,0 +1,9 @@
+package com.project.server.examples.async_commit_example;
+
+import com.project.server.examples.async_commit_example.annotations.RandomMessageGenSub0;
+import com.project.server.examples.async_commit_example.annotations.RandomMessageGenSub1;
+
+
+public class ExampleDaemon {
+
+}
